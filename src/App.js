7f103afcb3a308506c8f0f171a0c47351a75b667
Harsh <joshiharsh920@gmail.com>
2025-11-16 23:32:50 +0530

    Updated Module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import './css/App.css';
+import Favorites from './Pages/Favorites';
 import Home from "./Pages/Home"
+import { Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
 function App() {
 
 
   return (
-    <>
-    <Home />
-    </>
+    <div>
+      <NavBar/>
+   <main className='main-content'>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/favorites" element={<Favorites/>}></Route>
+    </Routes>
+   </main>
+   </div>
   );
 }
 
--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,6 +1,9 @@
-function Favorite(){
+import "../css/Favorites.css"
+function Favorites(){
     return <div className="favorite-empty">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies to your favorites and they will appear here.</p>
     </div>
 }
+
+export default Favorites;
